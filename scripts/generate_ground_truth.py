4b02a9ee520e0949d987e4809859f42e702f9dbf
Pietro Fanti <pietro.fanti@esa.int>
2025-03-18 17:24:28 +0100

    chore: save figures by default

--- scripts/generate_ground_truth.py
+++ scripts/generate_ground_truth.py
@@ -7,4 +7,4 @@ def mask_generator(mascon_points):
     return mascon_points[:, 0] - 0.5 * mascon_points[:, 2] > 0.4
 
 
-mesh_to_gt("itokawa_lp", mask_generator, mask_scalar)
+mesh_to_gt("itokawa_lp", mask_generator, mask_scalar, save_image=True)
